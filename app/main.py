4d84d6039a695775e8395002cd611cd1fa4aa49c
boscaEllis <elmcgu@proton.me>
2025-02-11 20:07:35 +0000

    echo command

--- app/main.py
+++ app/main.py
@@ -13,6 +13,14 @@ def main():
         if command.lower() == "exit 0":
             break
 
+        elif command.lower().split()[0] == "echo":
+            output_list = command.split()
+            count = len(output_list)
+            final_output = ""
+            for i in range (1,count):
+                final_output += f"{output_list[i]} "
+            print(final_output.rstrip())
+
         else:
             print(f"{command}: command not found")
 
